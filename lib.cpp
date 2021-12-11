5a1cf5851beb69c1ba1ae71a025a75cc17aec3e7
alexfragala <91506326+alexfragala@users.noreply.github.com>
2021-12-11 21:10:06 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+//
+// Created by Alex Fragalà on 09/12/21.
+//
+#include <iostream>
+#include "string.h"
+#include "lib.h"
 
+using namespace std;
+
+bool posizione(char A[10][20], char B[10], int &b){
+    for(int i=0; i<=10; i++){
+        if (strcmp(B, A[i])==0){
+            b=i;
+            return true;
+        }
+    }
+    return false;
+}
