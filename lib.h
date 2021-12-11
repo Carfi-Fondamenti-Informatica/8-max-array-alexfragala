86b3d82a5b59282f8b5f2c60300434211c086561
alexfragala <91506326+alexfragala@users.noreply.github.com>
2021-12-11 21:32:05 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,9 +2,8 @@
 // Created by Alex Fragalà on 09/12/21.
 //
 
-#ifndef ESERCITAZIONE9_LIB_H
-#define ESERCITAZIONE9_LIB_H
-#define LUNGHEZZA_MAX 100
-bool posizione(char A[10][20], char B[10], int &b);
-#endif //ESERCITAZIONE9_LIB_H
+#ifndef ESERCITAZIONE8_LIB_H
+#define ESERCITAZIONE8_LIB_H
 
+float maggiore(float numeri[], int dim);
+#endif //ESERCITAZIONE8_LIB_H
